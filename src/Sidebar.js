55cdcd3b078b4c1b9c328a65d7300896744dd76e
Vatsalya Singh Baghel <vatsalyasinghbaghel@Vatsalyas-MacBook-Air.local>
2024-03-15 19:19:40 +0530

    Sidebar

---
+++ src/Sidebar.js
@@ -0,0 +1,35 @@
+// Sidebar.js
+import React from "react";
+import "./Sidebar.css";
+import XIcon from "@mui/icons-material/X";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MailIcon from "@mui/icons-material/Mail";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { Button } from "@mui/material";
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <XIcon />
+      <SidebarOption active  Icon={HomeIcon} text="Home" />
+      <SidebarOption Icon={SearchIcon} text="Explore" />
+      <SidebarOption Icon={NotificationsIcon} text="Notification" />
+      <SidebarOption Icon={MailIcon} text="Messages" />
+      <SidebarOption Icon={BookmarkIcon} text="Bookmarks" />
+      <SidebarOption Icon={ListAltIcon} text="Lists" />
+      <SidebarOption Icon={PermIdentityIcon} text="Profile" />
+      <SidebarOption Icon={MoreHorizIcon} text="More" />
+      
+      
+      {/* Additional SidebarOptions */}
+      <Button variant="outlined" className="sidebar_tweet" fullWidth>Tweet</Button>
+    </div>
+  );
+}
+
+export default Sidebar;
